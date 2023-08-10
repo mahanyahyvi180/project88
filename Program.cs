6162b5208c27d80bca25971631c8ff175094fef2
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-10 15:40:55 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp88
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account myAccount = new Account();
+
+            Console.WriteLine($"Initial name is:{myAccount.GetName()}");
+
+            Console.Write("Enter the name:");
+            string theName = Console.ReadLine();
+
+            myAccount.SetName(theName);
+
+            Console.WriteLine($"myAccount's name is:{myAccount.GetName()}");
+        }
+    }
+
+    
+    }
+}
